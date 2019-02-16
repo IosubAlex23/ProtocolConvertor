b0c11bf60e46dfa46b69ca5637add2034192d197
Alex Iosub <alex_mihai7878@yahoo.com>
2019-02-16 23:16:06 +0200

    Added some masks for 16 bit registers

--- 02_SW/01_Master-Controller.X/03_Common/types.h
+++ 02_SW/01_Master-Controller.X/03_Common/types.h
@@ -18,6 +18,10 @@
 /* This is a mask used to clear the MS 4 bites of a byte */
 #define MASK_4MSB_CLEAR                                  (0x0F)
 #define MASK_8BIT_HALF                                   (4u)
+
+#define MASK_16BIT_HALF                                  (8u)
+#define MASK_8MSB_CLEAR                                  (0x00FF)
+
 #define MASK_FOR_CLEARING_BIT(bitPos)                    (~(1u << bitPos))
 #define MASK_FOR_SETTING_BIT(bitPos)                     (1u << bitPos)
 /* This returns the MS 4 bites of a byte */
@@ -27,6 +31,14 @@
 #define MASK_8BIT_CLEAR_BIT(reg, bitPosition)           (reg = reg & MASK_FOR_CLEARING_BIT(bitPosition))
 #define MASK_8BIT_SET_BIT(reg, bitPosition)             (reg = reg | MASK_FOR_SETTING_BIT(bitPosition))
 #define MASK_8BIT_GET_BIT(reg, bitPosition)             (reg & MASK_FOR_SETTING_BIT(bitPosition))
+
+/* This returns the MS 8 bites of a byte */
+#define MASK_16BIT_GET_MSB_HALF(x)                       (x >> MASK_16BIT_HALF)
+/* This returns the LS 8 bites of a byte */
+#define MASK_16BIT_GET_LSB_HALF(x)                       (x & MASK_8MSB_CLEAR)
+
+#define MASK_16BIT_MAX_VALUE                                 (65535u)
+#define MASK_16BIT_MAX_COUNTS                                (65536u)
 /*----------------------------------------------------------------------------*/
 /*                                 Data types                                 */
 /*----------------------------------------------------------------------------*/
