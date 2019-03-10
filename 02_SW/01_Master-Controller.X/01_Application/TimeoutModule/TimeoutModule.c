bdfd3f43c0f69ae9d41e66859ccaab7062d6c31a
ioan_nicoara <nicoara.i.ioan@gmail.com>
2019-03-10 17:04:17 +0200

    Added UART2 Module Added RS232 Module
    Updated UART1 Module

--- 02_SW/01_Master-Controller.X/01_Application/TimeoutModule/TimeoutModule.c
+++ 02_SW/01_Master-Controller.X/01_Application/TimeoutModule/TimeoutModule.c
@@ -1,132 +1,132 @@
-/**
- * \file       TimeoutModule.c
- * \author     Iosub Mihai Alexandru
- * \brief      This module is used for timeouts.
- */
-
-/*----------------------------------------------------------------------------*/
-/*                                 Includes                                   */
-/*----------------------------------------------------------------------------*/
-#include "TimeoutModule.h"
-/*----------------------------------------------------------------------------*/
-/*                               Local defines                                */
-/*----------------------------------------------------------------------------*/
-/* US = microseconds */
-#define US_CLK_SOURCE_VALUE         (0x03u)
-#define US_CLK_PRESCALER_POWER      (6u)
-#define MS_CLK_SOURCE_VALUE         (0x02u)
-#define MS_CLK_PRESCALER_POWER      (14u)
-/*----------------------------------------------------------------------------*/
-/*                              Local data types                              */
-/*----------------------------------------------------------------------------*/
-
-/*----------------------------------------------------------------------------*/
-/*                             Global data at RAM                             */
-/*----------------------------------------------------------------------------*/
-
-/*----------------------------------------------------------------------------*/
-/*                             Global data at ROM                             */
-/*----------------------------------------------------------------------------*/
-
-/*----------------------------------------------------------------------------*/
-/*                             Local data at RAM                              */
-/*----------------------------------------------------------------------------*/
-static TimeoutModule_State ModuleState;
-static uint8_t NumberOfOverflows;
-static Timer_Configuration timer_config;
-/*----------------------------------------------------------------------------*/
-/*                             Local data at ROM                              */
-/*----------------------------------------------------------------------------*/
-
-/*----------------------------------------------------------------------------*/
-/*                       Declaration of local functions                       */
-/*----------------------------------------------------------------------------*/
-/**
- * \brief     This function puts the TimeoutModule on IDLE and stops TMR0
- * \param     None
- * \return    None 
- */
-void TimeoutModule_vStop(void);
-/*----------------------------------------------------------------------------*/
-/*                     Implementation of global functions                     */
-
-/*----------------------------------------------------------------------------*/
-void TimeoutModule_vInit()
-{
-    Timer0_vInit();
-    NumberOfOverflows = 0;
-    ModuleState = TIMEOUT_IDLE;
-}
-
-TimeoutModule_State TimeoutModule_uiSetTimeout(TimeoutModule_MeasurementUnit unit, uint16_t timeAmount)
-{
-    TimeoutModule_State returnValue = TIMEOUT_ERROR;
-    if (TIMEOUT_IDLE == ModuleState)
-    {
-        if (TIMEOUT_uS == unit)
-        {
-            timer_config.clockSourceValue = US_CLK_SOURCE_VALUE; /* 64MHz */
-            timer_config.prescalerPower = US_CLK_PRESCALER_POWER; /* 2^6 => 1Mhz */
-            timer_config.postscalerValue = 1;
-
-        }
-        else if (TIMEOUT_mS == unit)
-        {
-            timer_config.clockSourceValue = MS_CLK_SOURCE_VALUE; /* 16MHz */
-            timer_config.prescalerPower = MS_CLK_PRESCALER_POWER; /* 2^14 => 976Hz */
-            timer_config.postscalerValue = 1; /* 976Hz (~1ms) */
-            NumberOfOverflows = 0;
-        }
-        timer_config.opMode = TIMER_16BIT_MODE;
-        timer_config.startValue = MASK_16BIT_MAX_VALUE - timeAmount;
-        Timer0_vStart(timer_config);
-        ModuleState = TIMEOUT_NOT_REACHED;
-        returnValue = TIMEOUT_NOT_REACHED;
-    }
-    else if (TIMEOUT_NOT_REACHED == ModuleState)
-    {
-        if (TIMEOUT_uS == unit)
-        {
-            if (true == Timer0_bWasOverflow())
-            {
-                returnValue = TIMEOUT_REACHED;
-                ModuleState = TIMEOUT_REACHED;
-            }
-        }
-        else if (TIMEOUT_mS == unit)
-        {
-            if (true == Timer0_bWasOverflow())
-            {
-//                NumberOfOverflows++;
-//                if (NumberOfOverflows >= timer_config.postscalerValue)
-//                {
-                    ModuleState = TIMEOUT_REACHED;
-                    returnValue = TIMEOUT_REACHED;
-//                }
-            }
-        }
-
-    }
-    else if (TIMEOUT_REACHED == ModuleState)
-    {
-        returnValue = TIMEOUT_IDLE;
-        TimeoutModule_vStop();
-    }
-    return returnValue;
-}
-
-TimeoutModule_State TimeoutModule_uiCancelTimeout(void)
-{
-    TimeoutModule_vStop();
-    return ModuleState;
-}
-/*----------------------------------------------------------------------------*/
-/*                     Implementation of local functions                      */
-
-/*----------------------------------------------------------------------------*/
-void TimeoutModule_vStop(void)
-{
-    ModuleState = TIMEOUT_IDLE;
-    NumberOfOverflows = 0;
-    Timer0_vStop();
-}
+///**
+// * \file       TimeoutModule.c
+// * \author     Iosub Mihai Alexandru
+// * \brief      This module is used for timeouts.
+// */
+//
+///*----------------------------------------------------------------------------*/
+///*                                 Includes                                   */
+///*----------------------------------------------------------------------------*/
+//#include "TimeoutModule.h"
+///*----------------------------------------------------------------------------*/
+///*                               Local defines                                */
+///*----------------------------------------------------------------------------*/
+///* US = microseconds */
+//#define US_CLK_SOURCE_VALUE         (0x03u)
+//#define US_CLK_PRESCALER_POWER      (6u)
+//#define MS_CLK_SOURCE_VALUE         (0x02u)
+//#define MS_CLK_PRESCALER_POWER      (14u)
+///*----------------------------------------------------------------------------*/
+///*                              Local data types                              */
+///*----------------------------------------------------------------------------*/
+//
+///*----------------------------------------------------------------------------*/
+///*                             Global data at RAM                             */
+///*----------------------------------------------------------------------------*/
+//
+///*----------------------------------------------------------------------------*/
+///*                             Global data at ROM                             */
+///*----------------------------------------------------------------------------*/
+//
+///*----------------------------------------------------------------------------*/
+///*                             Local data at RAM                              */
+///*----------------------------------------------------------------------------*/
+//static TimeoutModule_State ModuleState;
+//static uint8_t NumberOfOverflows;
+//static Timer_Configuration timer_config;
+///*----------------------------------------------------------------------------*/
+///*                             Local data at ROM                              */
+///*----------------------------------------------------------------------------*/
+//
+///*----------------------------------------------------------------------------*/
+///*                       Declaration of local functions                       */
+///*----------------------------------------------------------------------------*/
+///**
+// * \brief     This function puts the TimeoutModule on IDLE and stops TMR0
+// * \param     None
+// * \return    None 
+// */
+//void TimeoutModule_vStop(void);
+///*----------------------------------------------------------------------------*/
+///*                     Implementation of global functions                     */
+//
+///*----------------------------------------------------------------------------*/
+//void TimeoutModule_vInit()
+//{
+//    Timer0_vInit();
+//    NumberOfOverflows = 0;
+//    ModuleState = TIMEOUT_IDLE;
+//}
+//
+//TimeoutModule_State TimeoutModule_uiSetTimeout(TimeoutModule_MeasurementUnit unit, uint16_t timeAmount)
+//{
+//    TimeoutModule_State returnValue = TIMEOUT_ERROR;
+//    if (TIMEOUT_IDLE == ModuleState)
+//    {
+//        if (TIMEOUT_uS == unit)
+//        {
+//            timer_config.clockSourceValue = US_CLK_SOURCE_VALUE; /* 64MHz */
+//            timer_config.prescalerPower = US_CLK_PRESCALER_POWER; /* 2^6 => 1Mhz */
+//            timer_config.postscalerValue = 1;
+//
+//        }
+//        else if (TIMEOUT_mS == unit)
+//        {
+//            timer_config.clockSourceValue = MS_CLK_SOURCE_VALUE; /* 16MHz */
+//            timer_config.prescalerPower = MS_CLK_PRESCALER_POWER; /* 2^14 => 976Hz */
+//            timer_config.postscalerValue = 1; /* 976Hz (~1ms) */
+//            NumberOfOverflows = 0;
+//        }
+//        timer_config.opMode = TIMER_16BIT_MODE;
+//        timer_config.startValue = MASK_16BIT_MAX_VALUE - timeAmount;
+//        Timer0_vStart(timer_config);
+//        ModuleState = TIMEOUT_NOT_REACHED;
+//        returnValue = TIMEOUT_NOT_REACHED;
+//    }
+//    else if (TIMEOUT_NOT_REACHED == ModuleState)
+//    {
+//        if (TIMEOUT_uS == unit)
+//        {
+//            if (true == Timer0_bWasOverflow())
+//            {
+//                returnValue = TIMEOUT_REACHED;
+//                ModuleState = TIMEOUT_REACHED;
+//            }
+//        }
+//        else if (TIMEOUT_mS == unit)
+//        {
+//            if (true == Timer0_bWasOverflow())
+//            {
+////                NumberOfOverflows++;
+////                if (NumberOfOverflows >= timer_config.postscalerValue)
+////                {
+//                    ModuleState = TIMEOUT_REACHED;
+//                    returnValue = TIMEOUT_REACHED;
+////                }
+//            }
+//        }
+//
+//    }
+//    else if (TIMEOUT_REACHED == ModuleState)
+//    {
+//        returnValue = TIMEOUT_IDLE;
+//        TimeoutModule_vStop();
+//    }
+//    return returnValue;
+//}
+//
+//TimeoutModule_State TimeoutModule_uiCancelTimeout(void)
+//{
+//    TimeoutModule_vStop();
+//    return ModuleState;
+//}
+///*----------------------------------------------------------------------------*/
+///*                     Implementation of local functions                      */
+//
+///*----------------------------------------------------------------------------*/
+//void TimeoutModule_vStop(void)
+//{
+//    ModuleState = TIMEOUT_IDLE;
+//    NumberOfOverflows = 0;
+//    Timer0_vStop();
+//}
--- 02_SW/01_Master-Controller.X/01_Application/main.c
+++ 02_SW/01_Master-Controller.X/01_Application/main.c
@@ -1,45 +1,3 @@
-/**
-  Generated Main Source File
-
-  Company:
-    Microchip Technology Inc.
-
-  File Name:
-    main.c
-
-  Summary:
-    This is the main file generated using PIC10 / PIC12 / PIC16 / PIC18 MCUs
-
-  Description:
-    This header file provides implementations for driver APIs for all modules selected in the GUI.
-    Generation Information :
-        Product Revision  :  PIC10 / PIC12 / PIC16 / PIC18 MCUs - 1.65.2
-        Device            :  PIC18F26K83
-        Driver Version    :  2.00
- */
-
-/*
-    (c) 2018 Microchip Technology Inc. and its subsidiaries. 
-    
-    Subject to your compliance with these terms, you may use Microchip software and any 
-    derivatives exclusively with Microchip products. It is your responsibility to comply with third party 
-    license terms applicable to your use of third party software (including open source software) that 
-    may accompany Microchip software.
-    
-    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER 
-    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY 
-    IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS 
-    FOR A PARTICULAR PURPOSE.
-    
-    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
-    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
-    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP 
-    HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO 
-    THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL 
-    CLAIMS IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT 
-    OF FEES, IF ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS 
-    SOFTWARE.
- */
 
 #include <pic18.h>
 
@@ -47,7 +5,9 @@
 #include "../02_MCAL/GPIO/GPIO.h"
 #include "../02_MCAL/TIMER0/Timer0.h"
 #include "TimeoutModule/TimeoutModule.h"
-#include "../02_MCAL/UART/UART.h"
+//#include "../02_MCAL/UART/UART1.h"
+#include "../02_MCAL/UART/UART2.h"
+#include "../02_MCAL/RS232/RS232.h"
 
 /*
                          Main application
@@ -56,24 +16,35 @@ void main(void)
 {
     // Initialize the device
     SYSTEM_Initialize();
-    UART1_vInit();
-    UART1_vBaudCalculator(HIGH_SPEED,115200u);
-    UART1_vUARTMode(ASYNC_8BIT);
-    UART1_vStopBitMode(ONE_STOP_BIT);
-    UART1_vTransmitPolarityControl(INVERTED);
-
+    
+    RS232_actualConfig.communicationBaudGenSpeed = HIGH_SPEED;
+    RS232_actualConfig.communicationDesiredBaud = BAUD_9600;
+    RS232_actualConfig.communicationUartMode = ASYNC_8BIT;
+    RS232_actualConfig.communicationPolarity = NON_INVERTED;
+    RS232_actualConfig.communicationStopBitMode = ONE_STOP_BIT;
+    RS232_vInit(&RS232_actualConfig);
+    
+    UART2_vInit();
+    
     
 
     uint8_t i = 7;
-    char a  = 's';
+    uint8_t j = 8;
+
     while (1)
     {
-
-        UART1_uiTransmitter(1);
-
-        __delay_ms(10);
-       
-
+        UART2_uiTransmitter(i);
+        UART2_uiTransmitter('\r');
+        RS232_vSendData(j);
+        RS232_vSendData('\r');
+        if(STD_HIGH == RS232_bReceiveDataReady())
+        {
+            i = RS232_uiReceiveData();
+        }
+        if(STD_HIGH == UART2_bRX_Ready())
+        {
+            j = UART2_uiReception();
+        }        
     }
 }
 /**
